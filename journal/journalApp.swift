2ec64808a18a2dd6706719a247fb25775313662a
Mickson Bonsu Bossman <mbonsu69@outlook.com>
2024-03-06 21:15:10 +0100

    Initial Commit

---
+++ journal/journalApp.swift
@@ -0,0 +1,17 @@
+//
+//  journalApp.swift
+//  journal
+//
+//  Created by Mickson Bossman on 06.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct journalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
